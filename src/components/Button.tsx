7e2285c71a8c22eaa6a7d154834bb301a6c64972
Igor Potemkin <ipotemkin@rambler.ru>
2022-07-06 12:16:31 +0300

    Рождение кнопки

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,10 @@
-export const Button = () => {
-  return (
-    <>
-      <button>Кнопка</button>
-    </>
-  )
+import { FC } from 'react'
+import { cn } from '@bem-react/classname'
+
+const cnButton = cn('Button')
+
+type ButtonProps = {
+  text?: String
 }
+
+export const Button: FC<ButtonProps> = ({ text = "Кнопка" }) => <><button className={cnButton()}>{text}</button></>
