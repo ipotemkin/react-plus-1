693b94ec3af0ba082248b5f70c8d244393b5843d
Igor Potemkin <ipotemkin@rambler.ru>
2022-07-06 17:05:14 +0300

    removed useState from StateButton

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,15 @@ function App() {
       <br />
       {/* <div style={{ display: 'flex', gap: 16, flexWrap: 'wrap'}}> */}
         <StateButton />
-        <StateButton text="Button action m" buttonState="action" size="m" />
-        <StateButton text="Button outlined s" buttonState="outlined" size="s" />
-        <StateButton text="Buttontn outlined-info l" buttonState="outlined-info" size="l" />
-        <StateButton text="Button outlined-danger xl" buttonState="outlined-danger" size="xl" />
-        <StateButton text="Button rased" buttonState="raised" />
-        <StateButton text="Button flat" buttonState="flat" />
-        <StateButton text="Button flat-info" buttonState="flat-info" />
-        <StateButton text="Button flat-danger" buttonState="flat-danger" />
-        <StateButton text="Button flat-secondary" buttonState="flat-secondary" />
+        <StateButton text="Button action m" state="action" size="m" />
+        <StateButton text="Button outlined s" state="outlined" size="s" />
+        <StateButton text="Buttontn outlined-info l" state="outlined-info" size="l" />
+        <StateButton text="Button outlined-danger xl" state="outlined-danger" size="xl" />
+        <StateButton text="Button rased" state="raised" />
+        <StateButton text="Button flat" state="flat" />
+        <StateButton text="Button flat-info" state="flat-info" />
+        <StateButton text="Button flat-danger" state="flat-danger" />
+        <StateButton text="Button flat-secondary" state="flat-secondary" />
       {/* </div> */}
       </header>
     </div>
